d7928765bc16e07b4081ffd4680c6b71860ce65d
guilhermitoo <gh.dias1995@gmail.com>
2015-04-07 18:24:12 -0300

    criação de procedures e algumas alterações no diagrama de classe e fisico do banco

--- Banco de Dados/DDL.sql
+++ Banco de Dados/DDL.sql
@@ -29,12 +29,15 @@ create table pessoas
 	cpf			varchar(15) not null unique,
 	rg 			varchar(12),
 	nascimento	date,
+	telefone1	varchar(15),
+	telefone2	varchar(15),
 	sexo		char(1),
 	endereco	varchar(100),
 	usuario		varchar(20),
 	senha		varchar(50),
-	status		int,
-	obs		varchar(200),	
+	status		int check (status in (1,2)) not null, -- 1 = ativo, 2 = inativo
+	obs		varchar(200),
+	tipoUsuario int check(tipoUsuario in (1,2)), -- 1 = normal, 2 = administrador
 	cidade_id	int			not null	references cidades -- relacionamento	
 )
 go
@@ -61,16 +64,6 @@ create table dentistas
 )
 go
 
-create table telefones
-(
-	pessoa_id	int			not null,
-	numero		varchar(20) not null,
-	tipo		varchar(20),
-	primary key(pessoa_id,numero)
-)
-go
-
-
 create table medicamentos
 (
 	id				   int          not null primary key identity,
@@ -84,10 +77,10 @@ go
 
 create table imagens
 (
-	id				int			   not null,
+	id				int			   not null identity,
 	paciente_id		int			   not null	references pacientes,
+	descricao		varchar(50),	
 	arquivo			varbinary(max) not null,
-	descricao		varchar(50),
 	primary key		(id,paciente_id)
 )
 go
@@ -104,7 +97,7 @@ go
 create table tratamentos
 (
 	avaliacao_id int not null primary key references avaliacoes,
-	status int		 not null,
+	status int check(status in (1,2,3)) not null, -- 1 = Em Orçameto, 2 = Em Andamento, 3 = Concluído
 	total decimal(15,2)
 )
 go
@@ -122,7 +115,7 @@ create table itensTratamento
 	procedimento_id int not null references procedimentos,
 	qtd int not null,
 	valor decimal(15,2),
-	status int not null,
+	status int check(status in (1,2)) not null, -- 1 = pendente, 2 = concluído
 	primary key(tratamento_id,procedimento_id)
 )
 go
@@ -131,7 +124,7 @@ create table atendimentos
 (
 	id int not null primary key identity,
 	data date not null,
-	status int,
+	status int check(status in (1,2,3)) not null, -- 1 = Pendente, 2 = Cancelado, 3 = Concluído
 	dentista_id int not null references dentistas,
 	tratamento_id int not null references tratamentos
 )
--- Banco de Dados/Procedures.sql
+++ Banco de Dados/Procedures.sql
@@ -12,7 +12,9 @@ begin
 end
 
 -- TESTE
--- exec cadCidade 'pindamonhangaba','SP'
+exec cadCidade 'pindamonhangaba','SP'
+
+
 create procedure cadMedicamento
 (
 	@nome	varchar(100),
@@ -27,7 +29,7 @@ begin
 end
 
 -- teste
--- exec cadMedicamento 'Dipirona','antitêrmico','amarela','10 gotas','30 ml'
+exec cadMedicamento 'Dipirona','antitêrmico','amarela','10 gotas','30 ml'
 
 create procedure cadConvenio
 (
@@ -42,7 +44,8 @@ begin
 end
 
 -- teste
--- exec cadConvenio '080.000.000/0000-01','1234321','UNIMED LTDA','UNIMED'
+exec cadConvenio '080.000.000/0000-01','1234321','UNIMED LTDA','UNIMED'
+select * from pessoas
 
 create procedure cadPaciente
 (
@@ -50,24 +53,27 @@ create procedure cadPaciente
 	@cpf		varchar(15),
 	@rg 		varchar(12),
 	@nascimento	date,
+	@telefone1  varchar(15),
+	@telefone2	varchar(15),
 	@sexo		char(1),
 	@endereco	varchar(100),
 	@usuario	varchar(20),
 	@senha		varchar(50),
 	@status		int,
-	@obs		varchar(200),	
+	@obs		varchar(200),
+	@tipoUsuario int,	
 	@cidade_id	int,
 	@convenio   int
 )
 as
 begin
-	insert into pessoas values (@nome,@cpf,@rg,@nascimento,@sexo,@endereco,@usuario,@senha,@status,@obs,@cidade_id);
+	insert into pessoas values (@nome,@cpf,@rg,@nascimento,@telefone1,@telefone2,@sexo,@endereco,@usuario,
+											@senha,@status,@obs,@tipoUsuario,@cidade_id);
 	insert into pacientes values (@@IDENTITY,@convenio);
 end
 
 -- teste
--- exec cadPaciente 'Jairo Junio Tavares Pedro','123.123.321-22','123.123.123-1','24/06/1995','M','Rua de barro nº 2','jaja','juju',1,'',1
-
+exec cadPaciente 'Jairo','123.123.321-22','123.123.123-1','24/06/1995','1732323232','12323232123','M','Rua de barro nº 2','jaja','juju',1,'',1,1,1
 
 create procedure cadFuncionario
 (
@@ -75,18 +81,102 @@ create procedure cadFuncionario
 	@cpf		varchar(15),
 	@rg 		varchar(12),
 	@nascimento	date,
+	@telefone1  varchar(15),
+	@telefone2	varchar(15),
 	@sexo		char(1),
 	@endereco	varchar(100),
 	@usuario	varchar(20),
 	@senha		varchar(50),
 	@status		int,
 	@obs		varchar(200),	
+	@tipoUsuario int,
 	@cidade_id	int,
 	@salario	decimal(15,2),
 	@cargo		varchar(50)
 )
 as
 begin
-	insert into pessoas values(@nome,@cpf,@rg,@nascimento,@sexo,@endereco,@usuario,@senha,@status,@obs,@cidade_id);
-	insert into funcionarios values(@@IDENTITY,@salario,@cargo);
+	insert into pessoas values (@nome,@cpf,@rg,@nascimento,@telefone1,@telefone2,@sexo,@endereco,
+							@usuario,@senha,@status,@obs,@tipoUsuario,@cidade_id);
+insert into funcionarios values(@@IDENTITY,@salario,@cargo);
 end
+
+-- teste
+exec cadFuncionario 'Gui','123.123.321-23','123.123.123-1','24/06/1995','1732323232','12323232123','M','Rua de barro nº 2','jaja','juju',1,'',1,1,560.33,'programador'
+
+
+create procedure cadDentista
+(
+	@nome		varchar(100),
+	@cpf		varchar(15),
+	@rg 		varchar(13),  
+	@nascimento	date,
+	@telefone1  varchar(15),
+	@telefone2	varchar(15),
+	@sexo		char(1),
+	@endereco	varchar(100),
+	@usuario	varchar(20),
+	@senha		varchar(50),
+	@status		int,
+	@obs		varchar(200),	
+	@tipoUsuario int,
+	@cidade_id	int,
+	@cro		varchar(20)
+)
+as
+begin
+	insert into pessoas values (@nome,@cpf,@rg,@nascimento,@telefone1,@telefone2,@sexo,@endereco,
+							@usuario,@senha,@status,@obs,@tipoUsuario,@cidade_id);
+	insert into dentistas values (@@IDENTITY,@cro);
+end
+
+-- teste
+exec cadDentista'Guiga','123.123.321-25','123.123.123-1','24/06/1995','1732323232','12323232123','M','Rua de barro nº 2','jaja','juju',1,'',1,1,'32363'
+select * from pessoas
+
+
+create procedure cadImagem
+(
+	@paciente_id int,
+	@descricao varchar(50),
+	@arquivo varbinary(max)
+)
+as
+begin
+	insert into imagens values (@paciente_id,@descricao,@arquivo);
+end
+
+-- teste --
+exec cadImagem 1,'foto antes do tratamento',123123
+select * from imagens
+
+
+create procedure cadAvaliacao
+(
+	@data datetime,
+	@dentista_id int,
+	@paciente_id int
+)
+as
+begin
+	insert into avaliacoes values(@data,@dentista_id,@paciente_id);
+end
+
+-- teste
+exec cadAvaliacao '23/03/2015',4,1
+
+
+create procedure cadTratamento
+(
+	@avaliacao_id int,
+	@status int,
+	@total decimal(15,2)
+)
+as
+begin
+	insert into tratamentos values(@avaliacao_id,@status,@total);
+end
+
+-- teste
+exec cadTratamento 2,1,233.55
+select * from tratamentos

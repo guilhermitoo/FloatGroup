175cb867da194824ced08dc265abf3c7d446b31b
jjotatp <juniorjtp@hotmail.com>
2015-04-15 17:21:50 -0300

    implementação do ORM na classe medicamento conveneo

--- Projeto/Web_Odonto/BackEnd/Entity/Convenio.cs
+++ Projeto/Web_Odonto/BackEnd/Entity/Convenio.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.Linq.Mapping;
 namespace BackEnd.Entity
 {
+     [Table(Name = "convenios")]
     public class Convenio
     {
+        [Column(Name = "id", IsDbGenerated = true, IsPrimaryKey = true, CanBeNull = false)]
         public int Id { get; set; } // CHAVE PRIMÁRIA
+        [Column(Name = "razao_social")]
         public string RazaoSocial { get; set; }
+        [Column(Name="nome_fantasia")]
         public string NomeFantasia { get; set; }
+        [Column(Name = "cnpj")]
         public string Cnpj { get; set; }
+        [Column(Name = "ie")]
         public string Ie { get; set; }        
 
         public Convenio() { }
